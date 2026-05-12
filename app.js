6c1302baa1fdbd65e927aa06e5d8312eb80619e0
shubham34970 <shubham34970@gmail.com>
2026-05-12 12:28:53 +0530

    feat: Add new change to feature A

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/demo', (req, res) => {
-  res.json({ message: 'Hello from Feature A!' });
+  res.json({ message: 'Hello from Feature A! This is the new change' });
 });
 
 app.listen(port, () => {
